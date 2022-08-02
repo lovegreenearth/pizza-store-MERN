4f75b1b36291db89c20bc4383000e6443ff2ad59
Igor0825 <greenearth1019@gmail.com>
2022-08-02 20:10:55 +0200

    Update index.js

--- pizza-store-frontend/src/pages/SignUp/index.js
+++ pizza-store-frontend/src/pages/SignUp/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { LazyLoadImage } from 'react-lazy-load-image-component';
-import Button_1 from "../../components/Button/button1";
+import Button1 from "../../components/Button/button1";
 import images from '../../constant';
 
 
@@ -9,22 +9,97 @@ export default class SignUp extends Component {
         super(props);
 
         this.state = {
-            firstname: "",
-            lastname: "",
-            email: "",
-            password: "",
-            confirmpw: ""
+            firstNameAlert: "",
+            lastNameAlert: "",
+            emailAlert: "",
+            passwordAlert: "",
+            confirmpwAlert: ""
         }
 
         this.fnameRef = React.createRef();
         this.lnameRef = React.createRef();
-        this.email = React.createRef();
-        this.password = React.createRef();
-        this.confirmpw = React.createRef();
+        this.emailRef = React.createRef();
+        this.passwordRef = React.createRef();
+        this.confirmpwRef = React.createRef();
     }
 
-    onClick() {
+    onSignUp = () => {
+        const emailMatch = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(this.emailRef.current.value);
+        let flag = 0;
+        this.setState({
+            firstNameAlert: "",
+            lastNameAlert: "",
+            emailAlert: "",
+            passwordAlert: "",
+            confirmpwAlert: ""
+        })
 
+        if (!this.fnameRef.current.value){
+            this.setState({
+                firstNameAlert: "Please Enter First Name"
+            })
+            flag = 1;
+        }
+        if (!this.lnameRef.current.value){
+            this.setState({
+                lastNameAlert: "Please Enter Last Name"
+            })
+            flag = 1;
+        }
+        if (!this.emailRef.current.value || !emailMatch){
+            this.setState({
+                emailAlert: "Please Enter Valid Email"
+            })
+            flag = 1;
+        }
+        if (!this.passwordRef.current.value){
+            this.setState({
+                passwordAlert: "Please Enter Password"
+            })
+            flag = 1;
+        }
+        if (!this.confirmpwRef.current.value){
+            this.setState({
+                confirmpwAlert: "Please Confirm Password"
+            })
+            flag = 1;
+        }
+        if (this.passwordRef.current.value && this.passwordRef.current.value.length < 5){
+            this.setState({
+                passwordAlert: "Password must be more than 6 letters"
+            })
+            flag = 1;
+        }
+        if (this.passwordRef.current.value !== this.confirmpwRef.current.value){
+            this.setState({
+                confirmpwAlert: "Please Confrim Correct Password"
+            })
+            flag = 1;
+        }
+        if (flag === 0) {
+            const sendData = {
+                email: this.emailRef.current.value,
+                password: this.passwordRef.current.value,
+                passwordCheck: this.confirmpwRef.current.value,
+                displayName: this.fnameRef.current.value + ' ' + this.lnameRef.current.value
+            }
+            fetch(`http://localhost:5000/users/register`, {
+                method: 'POST',
+                headers: {
+                "Content-Type": "application/json"
+                },
+                body:JSON.stringify(sendData)
+            })
+            .then(res =>res.json())
+            .then(data => {
+                console.log('res------------------->',data.msg)
+                if (data.msg) {
+                    alert(data.msg);
+                } else if (data.role) {
+                    alert("success");
+                }
+            })
+        }
     }
 
     render() {
@@ -41,31 +116,31 @@ export default class SignUp extends Component {
                         <div className="email">
 
                             <div className="first-name">
-                                <div><label>First Name</label></div>
+                                <div><label>First Name<span>{this.state.firstNameAlert}</span></label></div>
                                 <div className="input"><input type="text" name="Ufirstname" id="Ufirstname" ref={this.fnameRef} /></div>
                             </div>
 
                             <div className="last-name">
-                                <div><label>Last Name</label></div>
+                                <div><label>Last Name<span>{this.state.lastNameAlert}</span></label></div>
                                 <div className="input"><input type="text" name="Ulastname" id="Ulastname" ref={this.lnameRef} /></div>
                             </div>
 
                             <div className="email-input">
-                                <div><label>Email Address</label></div>
+                                <div><label>Email Address<span>{this.state.emailAlert}</span></label></div>
                                 <div className="input"><input type="email" name="Uname" id="Uname" placeholder="" ref={this.emailRef} /></div>
                             </div>
 
                             <div className="password-input">
-                                <div><label>Password</label></div>
-                                <div className="input"><input type="password" name="Upassword" id="Upassword" placeholder="" /></div>
+                                <div><label>Password<span>{this.state.passwordAlert}</span></label></div>
+                                <div className="input"><input type="password" name="Upassword" id="Upassword" placeholder="" ref={this.passwordRef} /></div>
                             </div>
 
                             <div className="password-confirm">
-                                <div><label>Confirm Password</label></div>
-                                <div className="input"><input type="password" name="Uconfirmpassword" id="Uconfirmpassword" /></div>
+                                <div><label>Confirm Password<span>{this.state.confirmpwAlert}</span></label></div>
+                                <div className="input"><input type="password" name="Uconfirmpassword" id="Uconfirmpassword" ref={this.confirmpwRef} /></div>
                             </div>
-                            
-                            <Button_1 value="SIGN UP" onClick={this.onClick} />
+
+                            <Button1 value="SIGN UP" onClick={this.onSignUp}/>
                         </div>
                     </div>
                 </div>
