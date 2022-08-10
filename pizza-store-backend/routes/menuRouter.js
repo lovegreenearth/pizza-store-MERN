ca1050cdba9e8940c128010d9b8c17fe62440321
Igor0825 <greenearth1019@gmail.com>
2022-08-10 08:03:57 +0200

    Create menuRouter.js

---
+++ pizza-store-backend/routes/menuRouter.js
@@ -0,0 +1,39 @@
+const router = require("express").Router();
+const Menu = require("../models/menuModel");
+
+router.post("/addMenu", async (req, res) => {
+  try {
+    let { name } = req.body;
+
+    // validate
+
+    if (!name)
+      return res.status(400).json({ msg: "Not all fields have been entered." });
+    
+    const existingMenu = await Menu.findOne({ name: name });
+    if (existingMenu)
+      return res
+        .status(400)
+        .json({ msg: "A menu already exists." });
+
+    const newMenu = new Menu({
+      name
+    });
+    const savedMenu = await newMenu.save();
+    res.json(savedMenu);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+router.get("/", auth, async (req, res) => {
+  Menu.find({}, function (err, menus) {
+    if(err){
+        res.send(err);
+    }else{
+        res.send(menus);
+    }
+})
+});
+
+module.exports = router;
